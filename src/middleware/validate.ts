fe6dffb0047d17a5eb30eccf6802aab24114f645
aryankumar <aryankumar2003may@gmail.com>
2026-04-03 14:44:02 +0530

    middleware fix

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -12,7 +12,10 @@ export const validate = (schemas: Schemas) => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             if (schemas.body) req.body = schemas.body.parse(req.body)
-            if (schemas.query) req.query = schemas.query.parse(req.query) as typeof req.query
+            if (schemas.query) {
+                const parsedQuery = schemas.query.parse(req.query)
+                Object.assign(req.query, parsedQuery)
+            }
             if (schemas.params) req.params = schemas.params.parse(req.params) as typeof req.params
             next()
         } catch (err) {
